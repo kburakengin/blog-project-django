e9c19339c8931f92cecda4883608dbddd5243822
Kadir Burak Engin <kburakengin@gmail.com>
2020-06-18 15:45:43 +0300

    updated image size

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -9,3 +10,19 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+        cover  = Image.open(self.cover_photo.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
+        if cover.height > 700 or cover.width > 1000:
+            output_size = (500,900)
+            cover.thumnail(output_size)
+            cover.save(self.cover_photo.path)
