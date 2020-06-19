23691dd3df52a07a5e195d29225b5b8b87c07f53
Kadir Burak Engin <kburakengin@gmail.com>
2020-06-19 11:36:29 +0300

    changed update function with Updateview

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
     ListView,
     DetailView,
-    CreateView)
+    CreateView, UpdateView)
 from .models import Post
 
 
@@ -27,7 +26,7 @@ class PostDetailView(DetailView):
     model = Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'subtitle', 'content']
 
@@ -35,6 +34,14 @@ class PostCreateView(CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'subtitle', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
