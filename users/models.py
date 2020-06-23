f97120ac6856334d2d61d0b4338807b07421f8f1
Kadir Burak Engin <kburakengin@gmail.com>
2020-06-23 14:55:20 +0300

    adding AWS S3 file management

--- users/models.py
+++ users/models.py
@@ -11,18 +11,18 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        img = Image.open(self.image.path)
-        cover = Image.open(self.cover_photo.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
-
-        elif cover.height > 1267 or cover.width > 1900:
-            output_size = (1267, 1900)
-            cover.thumbnail(output_size)
-            cover.save(self.cover_photo.path)
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
+    #
+    #     img = Image.open(self.image.path)
+    #     cover = Image.open(self.cover_photo.path)
+    #
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
+    #
+    #     elif cover.height > 1267 or cover.width > 1900:
+    #         output_size = (1267, 1900)
+    #         cover.thumbnail(output_size)
+    #         cover.save(self.cover_photo.path)
