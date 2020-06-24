4d27b5351bead90bacf34a70ddc90cab2401202d
Kadir Burak Engin <kburakengin@gmail.com>
2020-06-24 12:40:41 +0300

    added upload_to file back

--- users/models.py
+++ users/models.py
@@ -5,8 +5,8 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg')
-    cover_photo = models.ImageField(default='defaultcover.jpg')
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    cover_photo = models.ImageField(default='defaultcover.jpg', upload_to='cover_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
